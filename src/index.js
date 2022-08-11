86284417858883a815cba919f228e764d54baa5d
gru6 <iliagrushevich@gmail.com>
2022-08-11 17:34:40 +0300

    brakets task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN_BRACKETS = ['(', '{', '[', '|', '1', '3', '5', '7', '8'];
+  const BRACKETS_PAIR = {
+    [')']: '(',
+    ['}']: '{',
+    ['|']: '|',
+    [']']: '[',
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5',
+    ['7']: '7',
+    ['8']: '8',
+  };
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+
+    if (BRACKETS_PAIR[currentSymbol] === topElement && stack.length > 0) {
+      stack.pop();
+    } else {
+      if (OPEN_BRACKETS.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+      }
+    }
+  }
+  return stack.length === 0;
 }
+
